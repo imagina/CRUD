1c26e1571abde48bd9f94560f08df40e19365d66
Carlos Devia <carlosdevia@imaginacolombia.com>
2017-01-13 16:00:47 -0500

    +Clean: Delete OLD src Folder

--- src/resources/views/columns/date.blade.php
+++
@@ -1,4 +0,0 @@
-{{-- localized date using jenssegers/date --}}
-<td data-order="{{ $entry->{$column['name']} }}">
-	{{ Date::parse($entry->{$column['name']})->format(config('backpack.base.default_date_format')) }}
-</td>
